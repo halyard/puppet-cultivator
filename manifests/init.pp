9e110515f5a6b8df89684487f2f4d0182f133612
Les Aker <me@lesaker.org>
2023-07-07 19:02:53 -0400

    update version

--- manifests/init.pp
+++ manifests/init.pp
@@ -9,7 +9,7 @@
 # @param frequency sets how often to run updates
 class cultivator (
   String $integration_id,
-  String $version = 'v0.0.14',
+  String $version = 'v0.0.16',
   String $dir = '/var/lib/cultivator',
   String $check_repo = 'https://github.com/akerl/repo-checks',
   String $bootdelay = '300',
